b8b11c7a1c68530e6e70e85919489b00a4854a37
Abby Gottlich <abby.gottlich@gmail.com>
2020-04-13 13:39:31 -0500

    Initial Commit

---
+++ Lunch Time/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lunch Time
+//
+//  Created by Abby Gottlich on 4/13/20.
+//  Copyright © 2020 Abby Gottlich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lunch TimeTests/Lunch_TimeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Lunch_TimeTests.swift
+//  Lunch TimeTests
+//
+//  Created by Abby Gottlich on 4/13/20.
+//  Copyright © 2020 Abby Gottlich. All rights reserved.
+//
+
+import XCTest
+@testable import Lunch_Time
+
+class Lunch_TimeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
